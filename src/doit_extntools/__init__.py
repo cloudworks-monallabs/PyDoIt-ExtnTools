c93dd141921ad2cc99b2eb6b37b13350c6dd426c
kabira <kabira@monallabs.in>
2024-05-29 10:17:15 +0530

    using given fabric_conn

--- src/doit_extntools/__init__.py
+++ src/doit_extntools/__init__.py
@@ -13,32 +13,28 @@ ssh_options = {
 class RemoteFilesDep(object):
     """Custom uptodate class to check the modification state of remote files using MD5 hash"""
 
-    def __init__(self, ipv6, remote_files, ssh_user="root"):
+    def __init__(self, fabric_conn,  remote_files):
         """
         unable to pickle fabric connection object:
         using ipv6
         """
 
-        self.ipv6 = ipv6
+        self.fabric_conn = fabric_conn
         self.remote_files = remote_files
-        # Compute the modified data md5 over files
-        self.ssh_user = ssh_user
         self.modified_state = self.compute_remote_files_md5()
 
     def compute_remote_files_md5(self):
         # Initialize an empty MD5 hash
-        fabric_conn = Connection(self.ipv6, user=self.ssh_user)
         combined_md5 = hashlib.md5()
         for remote_file in self.remote_files:
-            if exists(fabric_conn, remote_file):
+            if exists(self.fabric_conn, remote_file):
                 file_md5 = self.compute_file_md5(remote_file)
                 combined_md5.update(file_md5.encode())
         return combined_md5.hexdigest()
 
     def compute_file_md5(self, remote_file):
         # Run the md5sum command remotely to compute the MD5 hash
-        fabric_conn = Connection(self.ipv6, user=self.ssh_user)
-        result = fabric_conn.run(f'gmd5sum {remote_file}', hide=True)
+        result = self.fabric_conn.run(f'gmd5sum {remote_file}', hide=True)
         md5_hash = result.stdout.split()[0]
         return md5_hash
 
@@ -62,6 +58,6 @@ class RemoteFilesDep(object):
             print("in RemoteFileDep: dependency is uptodate")
 
             return True
-        print("in RemoteFileDep: dependency is not uptodate...run task to fetch files")
+        print("in RemoteFileDep: dependency is not uptodate...rerun task")
 
         return False
